48f7185c56953c4ad2b0cd5b9def65a47b1e332f
Kaylin Hsiao 2019 <khsiao9@3012-l29555-s.local>
2019-05-07 12:15:45 -0500

    Initial Commit

---
+++ magic8ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  magic8ball
+//
+//  Created by Kaylin Hsiao 2019 on 5/7/19.
+//  Copyright © 2019 Kaylin Hsiao 2019. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
