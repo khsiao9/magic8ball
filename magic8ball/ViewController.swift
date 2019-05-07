d75ce293ba7a14f8587f228db18c87c92f9133a0
Daniel Turchin 2021 <dturchin21@3012-l29552-s.local>
2019-05-07 12:38:35 -0500

    added daniel to pro

--- magic8ball/ViewController.swift
+++ magic8ball/ViewController.swift
@@ -3,6 +3,7 @@
 //  magic8ball
 // TEAM NAME: ******************
 //  Created by Kaylin Hsiao 2019 on 5/7/19.
+//  Created by Daniel Turchin 
 //  Copyright © 2019 Kaylin Hsiao 2019. All rights reserved.
 //
 
