0e15d027af0728e909221ee400c01689cb00bfaf
Jacqueline 2021 <jchen21@3012-l29534-s.local>
2019-05-08 12:33:19 -0500

    removed old image

--- magic8ball/ViewController.swift
+++ magic8ball/ViewController.swift
@@ -1,10 +1,10 @@
 //
 //  ViewController.swift
 //  magic8ball
-// TEAM NAME: ******************
+//  TEAM NAME: ******************
 //  Created by Kaylin Hsiao 2019 on 5/7/19.
-//Created by Alex Li on .....
-// Created by Jackie Chen
+//  Created by Alex Li on
+//  Created by Jackie Chen
 //  Copyright © 2019 Kaylin Hsiao 2019. All rights reserved.
 //
 
