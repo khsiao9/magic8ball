9471481c289669a59f69ffb0fb1f384b1d76e15a
Kaylin Hsiao 2019 <khsiao9@3012-l29555-s.local>
2019-05-09 12:31:29 -0500

    added user input outlet

--- magic8ball/SecondViewController.swift
+++ magic8ball/SecondViewController.swift
@@ -12,22 +12,44 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if(userQuestion.isEqual(""))
+        {
+            
+            
+        }
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func responseButtonTapped(_ sender: UIButton) {
+    @IBOutlet weak var userQuestion: UITextView!
+    @IBAction func responseButtonTapped(_ sender: UIButton)
+    {
         let computerChoice = Int.random(in: 1 ... 3)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+    
+    
+    
 
 }
